7dc3d8c1c4aca78da05e3a1ca0ee59c5ae476395
CEBracco <bracco66.cb@gmail.com>
2019-04-18 01:34:52 -0300

    Fix scroll arrow, add logic to change to dark icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  showLogo = false;
+  showLogo = true;
+  darkItems = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { SessionsComponent } from './components/sessions/sessions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SessionsComponent
   ],
   imports: [
     BrowserModule,
