156fa54425a02c30997ac5d00a1f7dee310b5e31
CEBracco <bracco66.cb@gmail.com>
2019-04-19 04:05:32 -0300

    Scroll effect with some animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as ScrollMagic from 'ScrollMagic';
+import 'ScrollMagic/scrollmagic/minified/plugins/debug.addIndicators.min.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  showLogo = true;
-  darkItems = true;
+export class AppComponent implements AfterViewInit {
+  showLogo = false;
+  darkItems = false;
+  
+  ngAfterViewInit(): void {
+    // get all slides
+    var slides = document.querySelectorAll("section");
+    this.setSlidesEffect(slides);
+    this.setOnShowFunction(slides);
+  }
+
+  setSlidesEffect(slides:any) {
+    var controller = new ScrollMagic.Controller({
+      globalSceneOptions: {
+        triggerHook: 'onLeave'
+      }
+    });
+
+    // create scene for every slide
+    for (var i = 0; i < slides.length; i++) {
+      new ScrollMagic.Scene({
+        triggerElement: slides[i]
+      })
+        .setPin(slides[i])
+        //.addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+    }
+    return controller;
+  }
+
+  setOnShowFunction(slides:any){
+    var appController = this;
+    var controller = new ScrollMagic.Controller();
+
+    // create scene for every slide
+    for (var i = 0; i < slides.length; i++) {
+      new ScrollMagic.Scene({
+        triggerElement: slides[i],
+        offset: '400%',
+        duration: '120%'
+      })
+        .on('enter', function (event) {
+          appController.setSectionIcons(event.target.triggerElement().id);
+        })
+        //.addIndicators()
+        .addTo(controller);
+    }
+    return controller;
+  }
+
+  setSectionIcons(id: String) {
+    // TODO: use component vars to configure icons
+    let sectionIconsConfig = [
+      { id: 'home', showLogo:false, darkItems: false },
+      { id: 'sessions', showLogo: true, darkItems: true }
+    ];
+    let config = sectionIconsConfig.find(config => config.id == id);
+    this.showLogo = config.showLogo
+    this.darkItems = config.darkItems;
+  }
 }
